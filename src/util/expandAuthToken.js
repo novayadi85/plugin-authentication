f0b5bd9cc809b60eee690d3d9e0cd3834fc04fbc
Komang Novayadi <novayadi85@gmail.com>
2020-07-16 11:54:24 +0800

    Update expandAuthToken.js

--- src/util/expandAuthToken.js
+++ src/util/expandAuthToken.js
@@ -10,15 +10,8 @@ import claimsCognito from "./decode-verify-jwt.js";
  * @returns {Object} JSON object
  */
 export default async function cognitoAuthToken(token) {
-  /*
-  const response = await fetch(HYDRA_OAUTH2_INTROSPECT_URL, {
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    method: "POST",
-    body: `token=${encodeURIComponent(token)}`
-  });
-  */
-
   const payloads = await claimsCognito({token});
+
   if (!payloads.isValid) throw new Error("Error introspecting token");
 
   return payloads;
